b6d5be77fdb0a64750bf28ec04b776ab565b52ec
st9540808 <st9540808@gmail.com>
2017-07-12 22:55:33 +0800

    add uint16_t version of bit_reverse()

--- Q1.c
+++ Q1.c
@@ -11,7 +11,18 @@ uint32_t bit_reverse_u32(uint32_t x)
     return n;
 }
 
+uint16_t bit_reverse_u16(uint16_t x)
+{
+    uint16_t mask = ~0, n = x;
+    for (int i = 3; i >= 0; i--) {
+        mask ^= mask << (1 << i);
+        n = ((n & ~mask) >> (1 << i)) | ((n & mask) << (1 << i));
+    }
+    return n;
+}
 
+
+// bit reverse first try, not use anymore
 uint32_t bit_reverse_recursive(uint32_t x, int length)
 {
     static int pos = 0;
@@ -33,19 +44,30 @@ uint32_t func(uint32_t x)
     return n;
 }
 
-void print_bit(uint32_t x)
+void print_bit_u32(uint32_t x)
 {
-    for (int i = 0; i < 32; i++) {
-        uint32_t temp = x & (1 << (31 - i));
-        printf("%d", temp != 0 ? 1 : 0);
-    }
+    for (int i = 0; i < 32; i++)
+        printf("%d", (x & (1 << 31 - i)) != 0 ? 1 : 0);
+    printf("\n");
+}
+
+void print_bit_u16(uint16_t x)
+{
+    for (int i = 0; i < 16; i++)
+        printf("%d", (x & (1 << 15 - i)) != 0 ? 1 : 0);
     printf("\n");
 }
 
 
 int main()
 {
-    int x = 1207959552;
-    print_bit(bit_reverse_u32(x));
-    print_bit(x);
+    uint32_t x = 1207959552;
+    print_bit_u32(x);
+    print_bit_u32(bit_reverse_u32(x));
+
+    printf("\n");
+
+    uint16_t y = 51232;
+    print_bit_u16(y);
+    print_bit_u16(bit_reverse_u16(y));
 }
